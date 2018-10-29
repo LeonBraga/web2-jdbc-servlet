6efc266de3e16fa15e5d781bd31e3661e86df71e
andfc <anderson.canel@bndes.gov.br>
2018-10-29 14:44:55 -0200

    banco de dados HSQLDB

--- src/com/suam/factory/ConnectionFactory.java
+++ src/com/suam/factory/ConnectionFactory.java
@@ -1,7 +1,5 @@
 package com.suam.factory;
 
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,6 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
 	/* Constantes com os parâmetros da conexão */
+	//MYSQL
 	private static final String URL = "jdbc:mysql://localhost:3306/dbweb2?useTimezone=true&serverTimezone=UTC";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
@@ -18,24 +17,21 @@ public class ConnectionFactory {
 		Connection conexao = null;
 		try {
 			/* Obtém o driver de conexão com o banco de dados */
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			//Class.forName("com.mysql.cj.jdbc.Driver");
 
 			/* Tenta se conectar */
-			conexao = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-			System.out.println("String de conexão: "+URL+USERNAME+PASSWORD);
+			//conexao = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			//System.out.println("String de conexão: "+URL+USERNAME+PASSWORD);
 			
 			/* Configura commit como NÃO Automático */
-			conexao.setAutoCommit(false);
+			//conexao.setAutoCommit(false);
 
 			/* Caso a conexão ocorra com sucesso, a conexão é retornada */
+			//conxao com HSQLDB
+			conexao = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/dbweb2", "SA", "");
 			return conexao;
 
-		} catch (ClassNotFoundException e) {
-			System.out.println("O driver expecificado nao foi encontrado.");
-			return null;
-		}
-
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Nao foi possivel conectar ao banco de dados.");
 			return null;
