fac4dbae85ad8783c516b44382e1126679f196ac
andfc <anderson.canel@bndes.gov.br>
2018-11-14 13:16:15 -0200

    correçaõ

--- src/com/suam/acao/AssentoOcupa.java
+++ src/com/suam/acao/AssentoOcupa.java
@@ -15,7 +15,7 @@ public class AssentoOcupa implements Acao {
 
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		String comprar = request.getParameter("comprar");
 		String idVoo = request.getParameter("idVoo");
 		String ocupa = request.getParameter("ocupa");
@@ -24,17 +24,21 @@ public class AssentoOcupa implements Acao {
 		String[] numeroAssento = request.getParameterValues("numeroAssento");
 		String[] numeroAssentoOcupado = request.getParameterValues("numeroAssentoOcupado");
 		Assento assent = new Assento();
-		Integer id = Integer.valueOf(idVoo);
+
+		Integer id = null;
+		if (idVoo != null) {
+			id = Integer.valueOf(idVoo);
+		}
 		Integer idOcupante = Integer.valueOf(ocupante);
 		String idVooVolta = request.getParameter("idVooVolta");
 		Integer idVolta = null;
-		if(idVooVolta != null) {
+		if (idVooVolta != null) {
 			idVolta = Integer.valueOf(idVooVolta);
 		}
 		String[] numeroAssentoVolta = request.getParameterValues("numeroAssentoVolta");
 		String[] numeroAssentoOcupadoVolta = request.getParameterValues("numeroAssentoOcupadoVolta");
-		
-		if (numeroAssento != null ) {
+
+		if (numeroAssento != null && idVoo != null) {
 			for (String assentoNum : numeroAssento) {
 				Integer numAssento = Integer.valueOf(assentoNum);
 				assent.setIdVoo(id);
@@ -53,9 +57,9 @@ public class AssentoOcupa implements Acao {
 				}
 			}
 		}
-		
-		//List<String> listaAssentosOcupados = new ArrayList<String>();
-		if (numeroAssentoOcupado != null ) {
+
+		// List<String> listaAssentosOcupados = new ArrayList<String>();
+		if (numeroAssentoOcupado != null && idVooVolta != null) {
 			for (String assentoNum : numeroAssentoOcupado) {
 				Integer numAssento = Integer.valueOf(assentoNum);
 				assent.setIdVoo(id);
@@ -68,16 +72,15 @@ public class AssentoOcupa implements Acao {
 				}
 				try {
 					AssentoService.update(assent);
-					//listaAssentosOcupados.add(assentoNum);
+					// listaAssentosOcupados.add(assentoNum);
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
-		
-		if(idVolta != null) {
-			if (numeroAssento != null ) {
+
+		if (idVolta != null) {
+			if (numeroAssento != null) {
 				for (String assentoNum : numeroAssentoVolta) {
 					Integer numAssento = Integer.valueOf(assentoNum);
 					assent.setIdVoo(idVolta);
@@ -96,9 +99,9 @@ public class AssentoOcupa implements Acao {
 					}
 				}
 			}
-			
-			//List<String> listaAssentosOcupados = new ArrayList<String>();
-			if (numeroAssentoOcupado != null ) {
+
+			// List<String> listaAssentosOcupados = new ArrayList<String>();
+			if (numeroAssentoOcupado != null) {
 				for (String assentoNum : numeroAssentoOcupadoVolta) {
 					Integer numAssento = Integer.valueOf(assentoNum);
 					assent.setIdVoo(idVolta);
@@ -111,24 +114,24 @@ public class AssentoOcupa implements Acao {
 					}
 					try {
 						AssentoService.update(assent);
-						//listaAssentosOcupados.add(assentoNum);
+						// listaAssentosOcupados.add(assentoNum);
 					} catch (SQLException e) {
 						e.printStackTrace();
 					}
 				}
 			}
-			return "redirect:entrada?acao=ListaAssento&vooId="+ idVoo+"&voltaId="+idVolta;
+			return "redirect:entrada?acao=ListaAssento&vooId=" + idVoo + "&voltaId=" + idVolta;
 		}
-		//QUANDO O USUÁRIO CLICAR EM COMPRAR SERÁ REDIRECIONADO PARA TELA DE COMPRA
-		/*if (comprar != null) {
-			for (String string : listaAssentosOcupados) {
-				System.out.println("ASSENTO" + string);
-			}
-			
-			request.setAttribute("numeroAssentoOcupado", listaAssentosOcupados);
-			return "forward:entrada?acao=FormNovaCompraVoo&compradorId="+idOcupante+"&idvoo="+id;
-		} */
-		
+		// QUANDO O USUÁRIO CLICAR EM COMPRAR SERÁ REDIRECIONADO PARA TELA DE COMPRA
+		/*
+		 * if (comprar != null) { for (String string : listaAssentosOcupados) {
+		 * System.out.println("ASSENTO" + string); }
+		 * 
+		 * request.setAttribute("numeroAssentoOcupado", listaAssentosOcupados); return
+		 * "forward:entrada?acao=FormNovaCompraVoo&compradorId="+idOcupante+"&idvoo="+
+		 * id; }
+		 */
+
 		return "redirect:entrada?acao=ListaAssento&vooId=" + assent.getIdVoo();
 	}
 }
